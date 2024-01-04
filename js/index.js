9d08b63ff591035a989f36d909ffd0cda1960f1d
Nadine Grantz <nadine.grantz@googlemail.com>
2024-01-04 18:05:08 +0100

    add solution

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+  console.log(event);
+
+  //   const firstName = data.firstName;
+  //   const age = Number(data.age);
+  //   const badness = Number(data.badness);
+
+  //   console.log(`The age-badness-sum of ${firstName} is ${age + badness}`);
+  //   // `The age-badness-sum of NADINE is 100`
+
+  event.target.reset();
+  event.target.elements.firstName.focus();
+});
